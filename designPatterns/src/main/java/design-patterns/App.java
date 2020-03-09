ba1e40136ecce6877c92608a0443aab3f24d7bc5
chandra mouli <chandramouli.garapati@gmail.com>
2020-03-09 21:26:15 +0530

    first commit

---
+++ designPatterns/src/main/java/design-patterns/App.java
@@ -0,0 +1,74 @@
+
+
+import java.util.logging.Logger;
+
+import java.util.logging.*;
+import java.util.logging.Level;
+
+interface Car {
+
+	public void assemble();
+}
+class BasicCar implements Car {
+
+	public void assemble() {
+		System.out.print("Basic Car.");
+	}
+
+}
+
+class CarDecorator implements Car {
+
+	protected Car car;
+	
+	public CarDecorator(Car c){
+		this.car=c;
+	}
+	
+	
+	public void assemble() {
+		this.car.assemble();
+	}
+
+}
+ class SportsCar extends CarDecorator {
+
+	public SportsCar(Car c) {
+		super(c);
+	}
+
+	
+	public void assemble(){
+		super.assemble();
+		System.out.print(" Adding features of Sports Car.");
+	}
+}
+ class LuxuryCar extends CarDecorator {
+
+	public LuxuryCar(Car c) {
+		super(c);
+	}
+	
+	
+	public void assemble(){
+		super.assemble();
+		System.out.print(" Adding features of Luxury Car.");
+	}
+}
+class App
+{
+    private static final Logger l=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    
+    public static void main( String[] args )
+    {
+       Car sportsCar = new SportsCar(new BasicCar());
+		sportsCar.assemble();
+		System.out.println("\n*****");
+		
+		Car sportsLuxuryCar = new SportsCar(new LuxuryCar(new BasicCar()));
+		sportsLuxuryCar.assemble();
+       l.log(Level.INFO,"using Loggers as singleton pattern");
+    }
+}
+
+
